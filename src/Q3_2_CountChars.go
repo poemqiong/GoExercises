3801f8c09fd34e2ec7e89fb00bb7280fbb32441a
poemqiong <poemqiong@163.com>
2015-09-02 21:45:04 +0800

    add: q3_2

---
+++ src/Q3_2_CountChars.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+import "unicode/utf8"
+
+func main() {
+    str := "asSASA ddd dsjkdsjs dk"
+    fmt.Printf("String %s\nLength: %d, Runes: %d\n",
+        str, len([]byte(str)), utf8.RuneCount([]byte(str)))    
+}
